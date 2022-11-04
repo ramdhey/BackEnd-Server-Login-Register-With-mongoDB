fee777d8b5a0aeb31ba8f42ab946fe06aff787cc
Rama Dhea Yudhistira <90076976+ramdhey@users.noreply.github.com>
2022-11-04 23:49:52 +0700

    update backend

--- server.js
+++ server.js
@@ -21,6 +21,24 @@ app.use(logger("dev"));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
+//Cors Configuration - Start
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested, Content-Type, Accept Authorization"
+  )
+  if (req.method === "OPTIONS") {
+    res.header(
+      "Access-Control-Allow-Methods",
+      "POST, PUT, PATCH, GET, DELETE"
+    )
+    return res.status(200).json({})
+  }
+  next()
+})
+//Cors Configuration - End
+
 
 // Import index router
 app.use("/", indexrouter);
