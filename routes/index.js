ad3fa25c2654cd8678eeb2fb2a6205349e5e2d9f
Rama Dhea Yudhistira <90076976+ramdhey@users.noreply.github.com>
2022-11-04 20:06:37 +0700

    sedikit

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require('express')
 var router =  express.Router()
 
 router.get("/",function(req,res,next){
-    res.send("Selamat datang di server register dan login yang dilindungi oleh JWT dan bcrypt")
+    res.send("API OKE")
 })
 
 module.exports= router
